de15e9810e20adb2141680dd72e8fb67a7d129f6
erik-ferreira <erikdesouzagf@gmail.com>
2023-10-12 15:43:18 -0300

    create Utils; fix in route update to not set null in properties

--- src/main/java/br/com/erikferreira/todolist/task/TaskController.java
+++ src/main/java/br/com/erikferreira/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erikferreira.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -61,7 +62,10 @@ public class TaskController {
 
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
