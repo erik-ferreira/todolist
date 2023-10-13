6eb5fc825749961d1f7fb47c638d66316855cec9
erik-ferreira <erikdesouzagf@gmail.com>
2023-10-13 14:07:13 -0300

    validation: task exists; user has permission to update task

--- src/main/java/br/com/erikferreira/todolist/task/TaskController.java
+++ src/main/java/br/com/erikferreira/todolist/task/TaskController.java
@@ -61,11 +61,24 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var task = this.taskRepository.findById(id).orElse(null);
+    var userId = request.getAttribute("userId");
+
+    if(task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("Tarefa não encontrada.");
+    }
+
+    if(!task.getUserId().equals(userId)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("Usuário não tem permissão para alterar essa tarefa.");
+    }    
 
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskSaved = this.taskRepository.save(task);
+
+    return ResponseEntity.ok().body(taskSaved);
   }
 }
